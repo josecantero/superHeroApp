24b35a596f9919e736a200d7b753217c2d8c19b1
josecantero <josedejesuscantero@gmail.com>
2024-09-26 19:21:29 -0400

    se agregó el binding y una depetencia para leer apis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/cabudev/superheros/MainActivity.kt
+++ app/src/main/java/com/cabudev/superheros/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.cabudev.superheros
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cabudev.superheros.databinding.ActivityMainBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,12 +21,35 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
-        binding.svSearchHero
+
+        initUI()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initUI(){
+        binding.svSearchHero.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun searchByName(query:String){
+        
+    }
+
+    private fun getRetrofit(): Retrofit {
+        val retrofit = Retrofit.Builder().baseUrl("https://superheroapi.com/").addConverterFactory(GsonConverterFactory.create()).build()
+        return retrofit
     }
 }
