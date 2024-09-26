3fe30659af102254597c19dec4dde443901f2907
josecantero <josedejesuscantero@gmail.com>
2024-09-26 11:32:34 -0400

    inicio del proyecto

---
+++ app/src/main/java/com/cabudev/superheros/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.cabudev.superheros
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cabudev.superheros.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.svSearchHero
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
